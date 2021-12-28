ad1ff987e771dfe09406218a68ea629b756cbb3a
Stephen Buchanan <stephen.buchanan@random.studio>
2021-12-28 14:22:06 +0100

    update tether agent

---
+++ README.md
@@ -0,0 +1,5 @@
+## Compiling instructions
+- `git submodule update --init --recursive`
+- `mkdir build && cd build`
+- `cmake ..`
+- `cmake --build .`
--- src/psn-tether-agent.cpp
+++ src/psn-tether-agent.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 
 #include "tether_agent.h"
+// #include <msgpack.hpp>
+
+
+// struct helloData {
+// 	std::string agentType;
+//   std::string agentID;
+// 	MSGPACK_DEFINE_MAP(agentType, agentID);
+// };
 
 using namespace std; 
 
 int main() {
+
+  const string agentType = "psn-bridge";
+  const string agentID = "test-psn-bridge";
+
   cout << "Tether PSN Agent starting..." << endl;
 
-
+  TetherAgent agent (agentType, agentID);
 
   return 0;
 }
